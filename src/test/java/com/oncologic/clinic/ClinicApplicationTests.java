fa83bd8c5c200c65267f60f0e9fc21d8908261e8
EstebanGZam <estebangaviria82@gmail.com>
2025-03-23 14:21:41 -0500

    chore: spring boot project initialized

---
+++ src/test/java/com/oncologic/clinic/ClinicApplicationTests.java
@@ -0,0 +1,12 @@
+package com.oncologic.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
