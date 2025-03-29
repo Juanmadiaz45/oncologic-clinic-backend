a6e539293c2705b08307f8ec4673afa23feb2bcb
EstebanGZam <estebangaviria82@gmail.com>
2025-03-29 10:36:10 -0500

    refactor: removal of unnecessary entities performed

--- src/main/java/com/oncologic/clinic/repository/personal/PersonalAvailabilityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oncologic.clinic.repository.personal;
-
-import com.oncologic.clinic.entity.personal.PersonalAvailability;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonalAvailabilityRepository extends JpaRepository<PersonalAvailability, PersonalAvailability.PersonalAvailabilityId> {
-}
