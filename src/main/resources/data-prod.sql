fbfb66cd3b69be44cec0d5523969a1c05a077b46
EstebanGZam <estebangaviria82@gmail.com>
2025-06-10 15:40:57 -0500

    fix: fixed permissions for the administrative

--- src/main/resources/data-prod.sql
+++ src/main/resources/data-prod.sql
@@ -54,7 +54,7 @@ VALUES (6, 1);
 INSERT INTO USER_ROLES (user_id, role_id)
 VALUES (7, 2);
 INSERT INTO USER_ROLES (user_id, role_id)
-VALUES (8, 5); -- labtech1 -> LAB_TECHNICIAN
+VALUES (8, 5);
 INSERT INTO USER_ROLES (user_id, role_id)
 VALUES (9, 4);
 INSERT INTO USER_ROLES (user_id, role_id)
@@ -87,6 +87,7 @@ INSERT INTO PERMISSIONS (id, name)
 VALUES (11, 'READ_OWN_DATA');
 
 -- Initializing ROLE_PERMISSIONS
+-- ADMIN permissions (all access)
 INSERT INTO ROLE_PERMISSIONS (role_id, permission_id)
 VALUES (1, 1);
 INSERT INTO ROLE_PERMISSIONS (role_id, permission_id)
@@ -107,8 +108,30 @@ INSERT INTO ROLE_PERMISSIONS (role_id, permission_id)
 VALUES (2, 9);
 INSERT INTO ROLE_PERMISSIONS (role_id, permission_id)
 VALUES (2, 10);
+
+-- PATIENT permissions (limited to own data)
 INSERT INTO ROLE_PERMISSIONS (role_id, permission_id)
 VALUES (3, 11);
+
+-- *** NEW: ADMINISTRATIVE permissions (administrative and appointment management) ***
+INSERT INTO ROLE_PERMISSIONS (role_id, permission_id)
+VALUES (4, 3); -- READ_PATIENTS
+INSERT INTO ROLE_PERMISSIONS (role_id, permission_id)
+VALUES (4, 4); -- WRITE_PATIENTS
+INSERT INTO ROLE_PERMISSIONS (role_id, permission_id)
+VALUES (4, 5); -- READ_APPOINTMENTS
+INSERT INTO ROLE_PERMISSIONS (role_id, permission_id)
+VALUES (4, 6); -- WRITE_APPOINTMENTS
+INSERT INTO ROLE_PERMISSIONS (role_id, permission_id)
+VALUES (4, 7); -- READ_TREATMENTS
+INSERT INTO ROLE_PERMISSIONS (role_id, permission_id)
+VALUES (4, 9); -- READ_EXAMINATIONS
+
+-- *** NEW: LAB_TECHNICIAN permissions (examination management) ***
+INSERT INTO ROLE_PERMISSIONS (role_id, permission_id)
+VALUES (5, 9); -- READ_EXAMINATIONS
+INSERT INTO ROLE_PERMISSIONS (role_id, permission_id)
+VALUES (5, 10); -- WRITE_EXAMINATIONS
 
 -- Initializing PERSONALS
 INSERT INTO PERSONAL (id, id_number, name, last_name, email, phone_number, date_of_hiring, status, user_id)
